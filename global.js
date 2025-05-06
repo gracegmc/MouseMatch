33a4bc95fcab7e052c24ac24541094ff56519e50
gracegmc <165077364+gracegmc@users.noreply.github.com>
2025-05-05 23:15:42 -0700

    customizing more layout/coloring in css

--- global.js
+++ global.js
@@ -1,8 +1,8 @@
 console.log('IT’S ALIVE!');
 
-function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
-}
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
 
 // BUILDING NAV BAR
 let pages = [
@@ -13,8 +13,8 @@ let pages = [
   ];
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-    ? "/projects/project3/MouseMatch/"                  // Local server
-    : "/MouseMatch/";         // GitHub Pages repo name
+    ? "/projects/project3/MouseMatch/"      // Local server
+    : "/MouseMatch/";                       // GitHub Pages repo name
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
@@ -37,42 +37,42 @@ for (let p of pages) {
         a.target = "_blank";
     } 
 
-    console.log(a.href)
     nav.append(a);
 }
 
-document.body.insertAdjacentHTML(
-    'afterbegin',
-    `
-      <label class="color-scheme">
-          Theme:
-          <select>
-              <option value="light dark">Automatic</option>
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-          </select>
-      </label>`,
-);
+// SETTING THEME
+// document.body.insertAdjacentHTML(
+//     'afterbegin',
+//     `
+//       <label class="color-scheme">
+//           Theme:
+//           <select>
+//               <option value="light dark">Automatic</option>
+//               <option value="light">Light</option>
+//               <option value="dark">Dark</option>
+//           </select>
+//       </label>`,
+// );
 
-// Function to set the color scheme to prevent redundant code
-function setColorScheme(colorScheme) {
-    // Apply the color scheme
-    document.documentElement.style.setProperty('color-scheme', colorScheme);
+// // Function to set the color scheme to prevent redundant code
+// function setColorScheme(colorScheme) {
+//     // Apply the color scheme
+//     document.documentElement.style.setProperty('color-scheme', colorScheme);
     
-    // Save the color-scheme preference to localStorage
-    localStorage.colorScheme = colorScheme;
-}
+//     // Save the color-scheme preference to localStorage
+//     localStorage.colorScheme = colorScheme;
+// }
 
-const select = document.querySelector('.color-scheme select');
+// const select = document.querySelector('.color-scheme select');
 
-if ("colorScheme" in localStorage) {
-    // Apply the saved preference
-    setColorScheme(localStorage.colorScheme);
+// if ("colorScheme" in localStorage) {
+//     // Apply the saved preference
+//     setColorScheme(localStorage.colorScheme);
     
-    // Update the select element to match
-    select.value = localStorage.colorScheme;
-}
+//     // Update the select element to match
+//     select.value = localStorage.colorScheme;
+// }
 
-select.addEventListener('input', function (event) {
-    setColorScheme(event.target.value)
-});
+// select.addEventListener('input', function (event) {
+//     setColorScheme(event.target.value)
+// });
