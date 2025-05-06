3befc6d13ddbdcc21ecc1276f4fa2e3c5d99b220
gracegmc <165077364+gracegmc@users.noreply.github.com>
2025-05-05 23:23:51 -0700

    removed the css changes since it didn't look good

--- global.js
+++ global.js
@@ -41,38 +41,38 @@ for (let p of pages) {
 }
 
 // SETTING THEME
-// document.body.insertAdjacentHTML(
-//     'afterbegin',
-//     `
-//       <label class="color-scheme">
-//           Theme:
-//           <select>
-//               <option value="light dark">Automatic</option>
-//               <option value="light">Light</option>
-//               <option value="dark">Dark</option>
-//           </select>
-//       </label>`,
-// );
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+              <option value="light dark">Automatic</option>
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+          </select>
+      </label>`,
+);
 
-// // Function to set the color scheme to prevent redundant code
-// function setColorScheme(colorScheme) {
-//     // Apply the color scheme
-//     document.documentElement.style.setProperty('color-scheme', colorScheme);
+// Function to set the color scheme to prevent redundant code
+function setColorScheme(colorScheme) {
+    // Apply the color scheme
+    document.documentElement.style.setProperty('color-scheme', colorScheme);
     
-//     // Save the color-scheme preference to localStorage
-//     localStorage.colorScheme = colorScheme;
-// }
+    // Save the color-scheme preference to localStorage
+    localStorage.colorScheme = colorScheme;
+}
 
-// const select = document.querySelector('.color-scheme select');
+const select = document.querySelector('.color-scheme select');
 
-// if ("colorScheme" in localStorage) {
-//     // Apply the saved preference
-//     setColorScheme(localStorage.colorScheme);
+if ("colorScheme" in localStorage) {
+    // Apply the saved preference
+    setColorScheme(localStorage.colorScheme);
     
-//     // Update the select element to match
-//     select.value = localStorage.colorScheme;
-// }
+    // Update the select element to match
+    select.value = localStorage.colorScheme;
+}
 
-// select.addEventListener('input', function (event) {
-//     setColorScheme(event.target.value)
-// });
+select.addEventListener('input', function (event) {
+    setColorScheme(event.target.value)
+});
